4607d92ca2b237bff98e994c22b2d858a028b78d
Bui Tran Thien An <buitranthienan1111@gmail.com>
2024-09-12 05:02:46 +0700

    create/global state with Zustand

---
+++ hook/useBookRoom.ts
@@ -0,0 +1,45 @@
+import { Room } from "@prisma/client";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface BookRoomStore {
+  bookingRoomData: RoomDataType | null;
+  paymentIntent: string | null;
+  clientSecret: string | undefined;
+  setRoomData: (data: RoomDataType) => void;
+  setPaymentIntent: (paymentIntent: string) => void;
+  setClientSecret: (clientSecret: string) => void;
+  resetBookRoom: () => void;
+}
+
+type RoomDataType = {
+  room: Room;
+  totalPrice: number;
+  breakfastIncluded: boolean;
+  startDate: Date;
+  endDate: Date;
+};
+
+const useBookRoom = create<BookRoomStore>(
+  persist(
+    (set) => ({
+      bookingRoomData: null,
+      paymentIntent: null,
+      clientSecret: undefined,
+      setRoomData: (data) => set(() => ({ bookingRoomData: data })),
+      setPaymentIntent: (paymentIntent) => set(() => ({ paymentIntent })),
+      setClientSecret: (clientSecret) => set(() => ({ clientSecret })),
+      resetBookRoom: () =>
+        set(() => ({
+          bookingRoomData: null,
+          paymentIntent: null,
+          clientSecret: undefined,
+        })),
+    }),
+    {
+      name: "BookRoom", // Corrected the syntax for the 'name' option.
+    }
+  )
+);
+
+export default useBookRoom;
